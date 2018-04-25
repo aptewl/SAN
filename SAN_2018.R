b9893a25371b365a86fe0f4d97ae2d76aff4f444
Laptew <brosat_suda@mail.ru>
2018-04-26 00:22:11 +0300

    Добавлено отображение результатов на графике boxplot

--- SAN_2018.R
+++ SAN_2018.R
@@ -1,13 +1,23 @@
+##install.packages("Cairo")
+##install.packages("ggplot2)
+
+###ТОЛЬКО ДЛЯ ПАРЫ ОБЪЕКТОВ!!!!
+
 
 ###################################
 ##########исходные данные##########
 ## сравниваются объекты Y1 и Y2
-## сраваниваются по 7 характеристикам X1, X2,...,X7
-## характеристики имеют коэфициенты важности M1, M2,...,M7
-## все характеристики с характером "больше-лучше"
+## сраваниваются по характеристикам X1, X2,...,Xk
+## характеристики имеют коэфициенты важности M1, M2,...,Mk
+## все характеристики с характером изменения свойства ТОЛЬКО "больше-лучше"
 ## количество итераций Монте-Карло n
+######################################
+## необходимые пакеты
+library(ggplot2)
 ###################################
 ##########функции##########
+
+##1 функция классифческого САН
 SAN_classic <- function(y1, y2, m){
   q1 <- 0 
   for(i in 1:length(y1)-1){
@@ -16,8 +26,8 @@ SAN_classic <- function(y1, y2, m){
   }
   return(q1)
 }
-
-SAN_algoritm <- function(y1, y2, m, n){
+##2 функция САН с применением метода Монте-Карло
+SAN_algoritm <- function(y1, y2, m, n, name_object){
   alpha_min <-c()
   qi_integral <- c()
   for(i in 1:length(y1)-1){
@@ -28,9 +38,8 @@ SAN_algoritm <- function(y1, y2, m, n){
   alpha <- cbind(alpha, runif(n, alpha_min[2], pi/2))
   alpha <- cbind(alpha, runif(n, alpha_min[3], pi/2))
   alpha <- cbind(alpha, runif(n, alpha_min[4], pi/2))
-  ## в цикле по монте-карло 
+  ##далее в цикле по монте-карло 
   for(k in 0:n){
-    
     q1 <- 0  
     for(i in 1:length(y1)-1){
       i<- i+1
@@ -39,12 +48,15 @@ SAN_algoritm <- function(y1, y2, m, n){
     qi_integral <- c(qi_integral,q1)
   }  
   ## конец цикла по монте-карло 
- # plot(density(qi_integral))
-  return(mean(qi_integral))
+  res_dat<-data.frame(VAL = qi_integral, OBJECT = name_object)
+  return((res_dat))
 }
+###############################
 ## ввод исходных данных
 Y1 <-c(3, 25, 15, 1)
 Y2 <-c(5, 30, 9, 0.8)
+#Y3 <-c(1, 17, 13, 0.5)
+#Y4 <-c(4, 2, 13, 0.5)
 M  <-c(0.1, 0.2, 0.5, 0.2)
 n  <-1000
 ###################################
@@ -58,7 +70,27 @@ print(SAN_classic(Y2, Y1, M))
 
 ### новый подход
 print("интегральный показатель объекта Y1 по новому методу: ")
-print(SAN_algoritm(Y1, Y2, M, n))
+print(mean(SAN_algoritm(Y1, Y2, M, n, 'Y1')$VAL ))
 print("интегральный показатель объекта Y2 по новому методу: ")
-print(SAN_algoritm(Y2, Y1, M, n))
+print(mean(SAN_algoritm(Y2, Y1, M, n, 'Y2')$VAL ))
+
+
+
+###отображение на графике boxplot
+result<-rbind(SAN_algoritm(Y1, Y2, M, n, 'Y1'), SAN_algoritm(Y2, Y1, M, n, 'Y2'))
+gr<-ggplot(data=result, aes(x=  OBJECT, y = VAL)) + 
+           geom_boxplot(aes(fill = OBJECT) )+ 
+           labs(x = "Сравниваемые объекты",
+                y = "Интегральный показатель превосходства")  + theme_minimal() 
+print(gr)#отобразить
+
+
+###сохранение графика boxplot в файл
+png(filename = "/home/.../file.png", type = "cairo", units = "px", 
+    width = 1600, height = 1200, res = 300, pointsize = 10)
+gr # записать  график в файл
+dev.off() # Отключить запись в файл и вернуть отображение графика в стандартном окне
+
+
+
 
